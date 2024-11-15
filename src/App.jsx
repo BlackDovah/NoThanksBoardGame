d9d18b69be880c31176adf37a21d73c848f8cf85
BlackDovah <132620985+BlackDovah@users.noreply.github.com>
2024-11-16 01:51:32 +0200

    updates for multiplayer

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,60 @@
-// import React from "react";
-// import { createRoot } from 'react-dom/client';
-import { Client } from 'boardgame.io/react';
-import { NoThanks } from './Game.js';
-import { NoThanksBoard } from './Board';
-// import { SocketIO } from 'boardgame.io/multiplayer'
-
+import React from "react";
+import { Client } from "boardgame.io/react";
+import { NoThanks } from "./Game.js";
+import { NoThanksBoard } from "./Board";
+import { SocketIO } from "boardgame.io/multiplayer";
+import "./output.css";
 
 const NoThanksClient = Client({
   game: NoThanks,
   numPlayers: 4,
   board: NoThanksBoard,
-  // multiplayer: SocketIO({ server: 'localhost:8000' }),
-})
+  multiplayer: SocketIO({ server: "localhost:8000" }),
+});
 
-// class App extends React.Component {
-//   state = { playerID: null };
+class App extends React.Component {
+  state = { playerID: null };
 
-//   render() {
-//     if (this.state.playerID === null) {
-//       return (
-//         <div>
-//           <p>Play as</p>
-//           <button onClick={() => this.setState({ playerID: "0" })}>
-//             Player 0
-//           </button>
-//           <button onClick={() => this.setState({ playerID: "1" })}>
-//             Player 1
-//           </button>
-//         </div>
-//       );
-//     }
-//     return (
-//       <div>
-//         <NoThanksClient playerID={this.state.playerID} />
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    if (this.state.playerID === null) {
+      return (
+        <div className="board text-6xl">
+          <p className="col-start-2 row-start-1 justify-self-center self-start">
+            Choose your seat :
+          </p>
+          <button
+            className="border-white bg-black p-3 col-start-1 row-start-1 justify-self-center self-center"
+            onClick={() => this.setState({ playerID: "0" })}
+          >
+            Player 0 (Red)
+          </button>
+          <button
+            className="border-white bg-black p-3 col-start-1 row-start-2 justify-self-center self-center"
+            onClick={() => this.setState({ playerID: "1" })}
+          >
+            Player 1 (Green)
+          </button>
+          <button
+            className="border-white bg-black p-3 col-start-3 row-start-1 justify-self-center self-center"
+            onClick={() => this.setState({ playerID: "2" })}
+          >
+            Player 2 (Blue)
+          </button>
+          <button
+            className="border-white bg-black p-3 col-start-2 row-start-1 justify-self-center self-center"
+            onClick={() => this.setState({ playerID: "3" })}
+          >
+            Player 3 (Yellow)
+          </button>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <NoThanksClient playerID={this.state.playerID} />
+      </div>
+    );
+  }
+}
 
-// const root = createRoot(document.getElementById("root"));
-// root.render(<App tab="home" />);
-
-export default NoThanksClient;
+export default App;
--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ import { NoThanks } from './Game.js';
 
 const server = Server({
   games: [NoThanks],
-  origins: [Origins.LOCALHOST],
+  origins: [
+    Origins.LOCALHOST_IN_DEVELOPMENT
+  ],
   // The server will automatically handle WebSocket connections, so no need to do anything extra
 });
 
-server.run(8000);  // Make sure to expose the WebSocket server on port 8000
+server.run(8000, () => console.log(`Server is runing on port 8000`));  // Make sure to expose the WebSocket server on port 8000
