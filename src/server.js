3bb3d7f927048db7b9af70e1b047198a128ab33f
BlackDovah <132620985+BlackDovah@users.noreply.github.com>
2024-11-17 22:49:48 +0200

    .

--- src/server.js
+++ src/server.js
@@ -1,24 +1,25 @@
 import { Server, Origins } from 'boardgame.io/dist/cjs/server.js';
 import path from 'path';
 import serve from 'koa-static';
-import { NoThanks } from './Game.js'; 
+import { NoThanks } from './Game.js';
 
 const server = Server({
   games: [NoThanks],
   origins: [
-    Origins.LOCALHOST_IN_DEVELOPMENT
+    Origins.LOCALHOST_IN_DEVELOPMENT, "https://enchanting-kashata-82a638.netlify.app"
   ],
 });
 
 const PORT = process.env.PORT || 8000;
 
 const frontEndAppBuildPath = path.resolve(__dirname, './dist');
-server.app.use(serve(frontEndAppBuildPath))
+// server.app.use(serve(frontEndAppBuildPath))
 server.run(PORT, () => {
   server.app.use(
     async (ctx, next) => await serve(frontEndAppBuildPath)(
       Object.assign(ctx, { path: 'index.html' }),
       next
     )
-  )
+  );
+  console.log(`Server running on port ${PORT}`);
 }); 
