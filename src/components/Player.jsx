d8028f0569960f06e806b9ac773041f0d2e8604d
BlackDovah <132620985+BlackDovah@users.noreply.github.com>
2024-11-18 21:10:05 +0200

    .

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,15 +3,18 @@ import { Card } from "./Card";
 
 export const Player = ({ playerID, tokens, cards, color }) => {
   return (
-      <div className={`player-${playerID} bg-gray-800 border-white flex items-center justify-center border-2 text-lg relative p-4 rounded-lg`}>
-        <div className="text-center xsm:text-xs">
-          Player{playerID}&nbsp;-&nbsp;Tokens:&nbsp;{tokens}&nbsp;-&nbsp;Cards:&nbsp;{cards.length}
-        </div>
-        <div className="held-cards">
-          {cards.map((card, index) => (
-            <Card key={index} card={card} color={color} />
-          ))}
-        </div>
+    <div
+      className={`group player-${playerID} bg-gray-800 border-white flex items-center justify-center border-2 text-lg relative p-4 rounded-lg`}
+    >
+      <div className="text-center xsm:text-xs">
+        Player{playerID}&nbsp;-&nbsp;Tokens:&nbsp;{tokens}
+        &nbsp;-&nbsp;Cards:&nbsp;{cards.length}
       </div>
+      <div className="held-cards hidden group-hover:block absolute bg-gray-700 p-2 rounded-lg shadow-lg">
+        {cards.map((card, index) => (
+          <Card key={index} card={card} color={color} />
+        ))}
+      </div>
+    </div>
   );
 };
