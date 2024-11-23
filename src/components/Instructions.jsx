a8d75a5ceb329a7e48775f0592709755fdc9624b
BlackDovah <132620985+BlackDovah@users.noreply.github.com>
2024-11-23 23:39:25 +0200

    .

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -22,11 +22,18 @@ export const Instructions = () => {
     <div
       className="absolute top-1/2 left-1/2 transform -translate-x-1/2 
     -translate-y-1/2 text-center bg-green-400 border-white border-8 p-8 
-    rounded-lg text-black font-bold text-shadow-custom"
+    rounded-lg text-black"
     >
-      <h1>How to Play No Thanks</h1>
+      <Link to="/">
+        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+          Back to Game
+        </button>
+      </Link>
+      <h1 className="text-xl">
+        <strong>How to Play No Thanks</strong>
+      </h1>
       <p>
-        <strong>Objective:</strong> Avoid points by strategically declining
+        <strong>Your Objective: </strong>Avoid points by strategically declining
         cards. The player with the fewest points wins.
       </p>
       <ol>
@@ -37,15 +44,59 @@ export const Instructions = () => {
         <li>
           <strong>2-</strong>If you pass, you must place one of your tokens on
           the card.
+          <br></br>
+          If you have no tokens, you will automatically take the card and all
+          tokens on it.
         </li>
         <li>
           <strong>3-</strong>If you take the card, you collect all tokens on it.
         </li>
         <li>
-          <strong>4-</strong>The game ends when all cards have been taken.
+          <strong>4-</strong>If you take cards with consecutive numbers, only
+          the lowest number in the series will count against you.
+          <br></br>A series can be made up of as few as two cards.
+        </li>
+        <li>
+          <strong>5-</strong>The game ends when all cards have been taken.
         </li>
       </ol>
-      <p>Good luck and have fun!</p>
+      <h1 className="text-xl">
+        <strong>Tips</strong>
+      </h1>
+      <ol>
+        <li>
+          <strong>1-</strong>It’s a good idea to try to hold onto tokens – they
+          help your score at the end of the game and they keep you from taking
+          cards you don’t want to take when you run out of tokens.
+        </li>
+        <li>
+          <strong>2-</strong>If a card that you need for a series has a high
+          number, you may want to let it go around a few times before you take
+          it so that the other players put tokens onto it before you take it.
+          <br></br>
+          Even if one of the other players takes it, s/he may be adding a lot of
+          points to his/her score (which helps you).
+        </li>
+        <li>
+          <strong>3-</strong>Pay attention to which players are running low on
+          tokens – they may be forced to take a card that you want when they run
+          out.
+        </li>
+      </ol>
+      <h1>
+        <strong>End game calculations</strong>
+      </h1>
+      <p>
+        At the end of the game, the values of your cards will be added together,
+        and your remaining tokens will be subtracted from the total score.
+        <br></br>
+        For cards that are in series, e.g: 30, 31, 32, only the first card in
+        the series will be added to the total score when adding the card values
+        together.
+      </p>
+      <p>
+        <strong>Good luck and have fun!</strong>
+      </p>
       <Link to="/">
         <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
           Back to Game
